863546c4268e46ed028ceec2a3b6858ac7e1c32a
Rob Povey <polygonhell@gmail.com>
2016-03-12 14:27:27 -0800

    Actually make it generate  code suitable for the processor

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import Data.List
 import Data.Char
 import Data.Maybe
 import Data.Word
+import Data.Bits
+import Text.Printf
 
 data Instruction = Imm Int 
                  | Branch Int | BranchTarget Int 
@@ -25,7 +27,10 @@ data Env = Env { base :: [FWord],
                  user :: [FWord]
                } deriving (Show)
 
-type GenEnv = [(String, Int)]
+data GenEnv = GenEnv { geAddr :: Int
+                     , symbols :: [(String, Int)]
+                     , locals :: [(Int, Int)]
+                     }
 
 
 baseWords = [ FWord "+" [Add] True
@@ -36,19 +41,56 @@ baseWords = [ FWord "+" [Add] True
             ]
 
 
+addInst = 0x6303 :: Word16
+dupInst = 0x6031 :: Word16
+retInst = 0x6004 :: Word16
+jmpInst = 0x2000 :: Word16
+callInst = 0x0000 :: Word16
+
+
+findLocalAddress :: GenEnv -> Int -> Word16
+findLocalAddress GenEnv{..} item = addr where
+    cmpfn a = fst a == item
+    addr' = find cmpfn locals
+    addr = fromIntegral $ snd $ fromMaybe (0, 0) addr'
+
+findSymbolAddress :: GenEnv -> String -> Word16
+findSymbolAddress GenEnv{..} item = addr where
+    cmpfn a = fst a == item
+    addr' = find cmpfn symbols
+    addr = fromIntegral $ snd $ fromMaybe (error (printf "couldn't find symbol %s" item)) addr'
+
+
+incAddr :: GenEnv -> GenEnv
+incAddr env@GenEnv{..} = env {geAddr = geAddr + 1}
+
 generateBytes :: GenEnv -> [Instruction] -> [Word16]
--- Tail Call
-generateBytes sym (Call t : Return: []) = [0x4000 + 54] -- Tail call becomes jump
+generateBytes sym [] = []
 
-generateBytes sym (Imm x : is) = fromIntegral x : generateBytes sym is
+generateBytes sym (Call t : Return : is) = jmpInst + loc : generateBytes (incAddr sym) is where -- Tail call becomes jump
+    loc = findSymbolAddress sym t
+generateBytes sym (Return : is) = retInst : generateBytes (incAddr sym) is
 
-generateBytes sym (Add : is) = 0x1234 : generateBytes sym is
+-- Not a real instruction, just a target for a branch
+generateBytes sym@GenEnv{..} (BranchTarget x : is) = generateBytes sym' is where
+    sym' = sym {locals = (x, geAddr) : locals}
 
-generateBytes sym (Dup : is) = 0x2345 : generateBytes sym is
 
-generateBytes sym (Call t : is) = 0x6000 + 54 : generateBytes sym is
-generateBytes sym (Return:[]) = [0x0600] 
-generateBytes sym inst = []
+generateBytes sym@GenEnv{..} (Branch x : is) = jmpInst + loc : generateBytes (incAddr sym) is where
+    loc = findLocalAddress sym x
+
+generateBytes sym@GenEnv{..} (Call t : is) = callInst + loc : generateBytes (incAddr sym) is where
+    loc = findSymbolAddress sym t
+
+
+generateBytes sym@GenEnv{..} (Imm x : is) = (0x8000 .|. fromIntegral x) : generateBytes (incAddr sym) is
+
+generateBytes sym@GenEnv{..} (Add : is) = addInst : generateBytes (incAddr sym) is
+
+generateBytes sym@GenEnv{..} (Dup : is) = dupInst : generateBytes (incAddr sym) is
+
+generateBytes _ inst = error $ printf "Unknown Instruction %s" $ show inst
+
 
 codeGenWord :: GenEnv -> FWord -> (String, [Word16])
 codeGenWord env (FWord n is _) = (n, generateBytes env is)
@@ -85,7 +127,30 @@ compileDefinition (t:ts) env  = newWord where
     newWord = FWord t instructions False
 
 getDefinitions :: [String] -> [[String]]
-getDefinitions tokens = splitOn [":"] tokens
+getDefinitions  = splitOn [":"] 
+
+
+compile :: GenEnv -> [FWord] -> (GenEnv, [Word16])
+compile env [] = (env, [])
+compile env@GenEnv{..} (w:ws) = (env'', wOut ++ wsOut) where
+    wOut = snd $ codeGenWord env w
+    env' = env { geAddr = geAddr + length wOut, symbols = (fwName w, geAddr) : symbols }
+    (env'', wsOut) = compile env' ws
+
+word16toHex :: [Word16] -> String
+word16toHex [] = ""
+word16toHex (w : ws) = hex ++ word16toHex ws where
+    hex = printf "%04x " w
+
+word16toBinLines :: [Word16] -> String
+word16toBinLines ws = unlines $ map fn (pairWords ws) where
+    fn = printf "%032b"
+    pairWords (w1 : w2 : ws) = toInteger w1 `shiftL` 16 + toInteger w2 : pairWords ws
+    pairWords [w1] = [toInteger w1 `shiftL` 16]
+    pairWords [] = []
+
+bootCode :: [Instruction]
+bootCode = [Call "main", BranchTarget 0, Branch 0]
 
 main ::  IO ()
 main = do
@@ -101,9 +166,16 @@ main = do
 
     print compiledDefs
     
-    let compiled = map (codeGenWord []) compiledDefs
+    let (env, uCompiled) = compile (GenEnv 2 [] []) compiledDefs
+        boot = generateBytes (env {geAddr = 0}) bootCode
+        compiled = boot ++ uCompiled
 
-    print compiled
+
+
+    print $ symbols env
+    print $ word16toHex compiled
+    putStr $ word16toBinLines compiled
+
 
 
 
